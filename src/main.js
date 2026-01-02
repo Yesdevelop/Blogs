6d8f51432c0c2d84ed3d3b04e1149727f6b7d153
ForYes <yes-noob@outlook.com>
2026-01-02 15:14:43 +0800

    improve ui

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
 import "./style.css"
+
+const LAYOUT_HOME = 0;
+const LAYOUT_ARTICLE = 1;
+const LAYOUT_ARCHIVES = 2;
+const LAYOUT_ABOUT = 3;
+
+function cloneTemplate(templateSelector, targetSelector) {
+    const HOME_TEMPLATE = document.querySelector(templateSelector);
+    const CONTENT = HOME_TEMPLATE.content.cloneNode(true);
+    document.querySelector(targetSelector).appendChild(CONTENT);
+}
+
+class Blogs {
+    layout = LAYOUT_HOME;
+    url = "/";
+
+    refresh() {
+        if (this.layout == LAYOUT_HOME) {
+            cloneTemplate("#home-view", ".view");
+        }
+    }
+}
+
+function main() {
+    let blogs = new Blogs();
+    blogs.refresh();
+}
+
+main();
