290ffca3beac9c45c3d5c2ed6b6dbf0dcd6caf02
ForYes <yes-noob@outlook.com>
2026-01-02 20:29:13 +0800

    inject

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,55 @@
-import fs from "fs"
-import path from "path"
-import { defineConfig } from "vite"
-import { createHtmlPlugin } from "vite-plugin-html"
-import { viteSingleFile } from "vite-plugin-singlefile"
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // 获取文章的预览内容
-function getPostsPreviews() {
-    const postsDir = path.resolve(__dirname, 'posts')
-    const files = fs.readdirSync(postsDir)
+function getPosts() {
+    const POSTS_DIR = "posts";
+    const LENGTH = 150;
 
-    return files
-        .filter(file => file.endsWith('.md'))
-        .map(file => {
-            const content = fs.readFileSync(path.join(postsDir, file), 'utf-8')
-            return {
-                slug: file.replace('.md', ''),
-                title: "这是标题",
-                preview: content.substring(0, 200) + '...'
-            }
-        })
+    const postsDir = path.resolve(__dirname, POSTS_DIR);
+    const files = fs.readdirSync(postsDir).filter(file => file.endsWith(".md"));
+    const ret = files.map((file) => {
+        const fileContent = fs.readFileSync(path.join(postsDir, file), "utf-8");
+        const { data, content } = matter(fileContent);
+
+        const preview = content.replaceAll(/\r|\n/g, "").substring(0, LENGTH) + "...";
+        const { title, lang, tags, date, updated } = data;
+        return { title, lang, tags, date, updated, preview };
+    });
+    return ret;
 }
 
+// 注入posts参数到js里
+function injectPosts() {
+    const PLUGIN_NAME = "PostInjector";
+    const PLACEHOLDER = "/* VITE_POSTS_INJECTOR */";
+    const VARIABLE_NAME = "posts";
+
+    return {
+        name: PLUGIN_NAME,
+        transformIndexHtml(html) {
+            const posts = getPosts();
+            const injectContent = `window.${VARIABLE_NAME} = ${JSON.stringify(posts)};`;
+            return html.replace(PLACEHOLDER, injectContent);
+        }
+    };
+}
+
+// 导出配置
 export default defineConfig({
     plugins: [
-        {
-            name: 'inject-posts',
-            transformIndexHtml(html) {
-                const posts = getPostsPreviews()
-                return html.replace(
-                    '<!-- POSTS_PLACEHOLDER -->',
-                    JSON.stringify(posts)
-                )
-            }
-        },
-        createHtmlPlugin({
-            minify: true
-        }),
-        viteSingleFile()
+        // 把posts内的文章注入页面中
+        injectPosts(),
+        // 压缩html
+        createHtmlPlugin({ minify: true, }),
+        // 合并单文件
+        viteSingleFile(),
     ],
     build: {
-        cssCodeSplit: false
+        cssCodeSplit: false,
     }
-})
+});
