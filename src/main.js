cd681a0c673696fec5d836600cded71f1ac80394
ForYes <yes-noob@outlook.com>
2026-01-10 23:40:27 +0800

    .

--- src/main.js
+++ src/main.js
@@ -5,24 +5,40 @@ const LAYOUT_ARTICLE = 1;
 const LAYOUT_ARCHIVES = 2;
 const LAYOUT_ABOUT = 3;
 
+window.layout = LAYOUT_HOME;
+
 function cloneTemplate(templateSelector, targetSelector) {
     const homeTemplate = document.querySelector(templateSelector);
+    const target = document.querySelector(targetSelector);
     const content = homeTemplate.content.cloneNode(true);
-    document.querySelector(targetSelector).appendChild(content);
+    target.appendChild(content);
+    return target;
 }
 
-class Blogs {
-    layout = LAYOUT_HOME;
-    loadPage() {
-        if (this.layout == LAYOUT_HOME) {
-            cloneTemplate("#home-view", ".view");
+function loadPage() {
+    if (window.layout == LAYOUT_HOME) {
+        cloneTemplate("#home-view", ".view");
+        loadPosts();
+    }
+}
+
+function loadPosts() {
+    console.log(1);
+    if (window.layout == LAYOUT_HOME) {
+        if (window.posts) {
+            for (const post of window.posts) {
+                const target = cloneTemplate("#post", ".main");
+                const last = target.querySelector(".post:last-child");
+                last.innerHTML = last.innerHTML.replace("%title%", post.title);
+            }
+        } else {
+            setTimeout(loadPosts, 50);
         }
     }
 }
 
 function main() {
-    const blogs = new Blogs();
-    blogs.loadPage();
+    loadPage();
 }
 
 main();
