490cb56b1b356869ef9e1a726c0347fd8c9d8e4e
ForYes <yes-noob@outlook.com>
2026-01-23 20:01:44 +0800

    ejs

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,33 @@
 import { defineConfig } from "vite";
 import { ViteEjsPlugin as createEjsPlugin } from "vite-plugin-ejs";
-import { resolve } from "path";
 
 export default defineConfig({
     plugins: [
         createEjsPlugin({
+            author: "ForYes",
+            bio: "Make it Awesome",
+            sitetitle: "ForYes",
+            subtitle: "Years rolling by just like a dream",
+            posts: [
+                {title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you"},
+                {title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you"},
+                {title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you"},
+                {title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you"},
+                {title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you"},
+                {title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you"},
+                {title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you"},
+                {title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you"},
+                {title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you"},
+                {title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you"},
+                {title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you"},
+            ],
+            postNumber: 0,
+            tagNumber: 0,
+            dayNumber: 0,
+            lastUpdateDate: "2026/1/1",
         }),
     ],
     build: {
-        outDir: "./docs"
+        outDir: "./docs",
     },
 });
