8c18619e3da95cd231c9efc0b69328fdbfe48647
ForYes <yes-noob@outlook.com>
2026-01-24 19:14:45 +0800

    dev

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Mask
 let maskDisplay = false;
 window.maskOnclickEvents = [];
 window.toggleMask = () => {
@@ -11,3 +12,32 @@ window.toggleMask = () => {
         maskDisplay = true;
     }
 }
+
+// app
+const ABOUT = 0;
+const ARCHIVES = 1;
+const HOME = 2;
+const POST = 3;
+const SEARCH = 4;
+
+const MAIN_ABOUT_TEMPLATE = document.getElementById("main-about");
+const ASIDE_ABOUT_TEMPLATE = document.getElementById("main-about");
+const MAIN_ARCHIVES_TEMPLATE = document.getElementById("main-archives");
+const ASIDE_ARCHIVES_TEMPLATE = document.getElementById("aside-archives");
+const MAIN_HOME_TEMPLATE = document.getElementById("main-home");
+const ASIDE_HOME_TEMPLATE = document.getElementById("aside-home");
+const MAIN_POST_TEMPLATE = document.getElementById("main-post");
+const ASIDE_POST_TEMPLATE = document.getElementById("aside-post");
+const MAIN_SEARCH_TEMPLATE = document.getElementById("main-search");
+const ASIDE_SEARCH_TEMPLATE = document.getElementById("aside-search");
+
+let layout = HOME;
+
+function changeLayout() {
+    if (layout === HOME) {
+        document.querySelector("main").appendChild(MAIN_HOME_TEMPLATE.content.cloneNode(true));
+        document.querySelector("aside").appendChild(ASIDE_HOME_TEMPLATE.content.cloneNode(true));
+    }
+}
+
+changeLayout();
--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,60 @@
-import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
+import fs from "fs";
+import { viteSingleFile as createSinglefilePlugin } from "vite-plugin-singlefile";
 import { ViteEjsPlugin as createEjsPlugin } from "vite-plugin-ejs";
+import matter from "gray-matter";
+import { marked } from "marked";
 
-export default defineConfig({
+const POSTS_DIR = "_posts/";
+
+class Post {
+    title;
+    date;
+    updated;
+    tags;
+    lang;
+    excerpt;
+    content;
+    constructor(post) {
+        const { data, content } = matter(post);
+        this.title = data.title;
+        this.date = data.date;
+        this.updated = data.updated;
+        this.tags = data.tags;
+        this.lang = data.lang;
+        this.excerpt = data.excerpt;
+        this.content = content;
+    };
+};
+
+function loadPosts() {
+    let ret = [];
+    const files = fs.readdirSync(POSTS_DIR);
+    files.forEach(filename => {
+        const filePath = POSTS_DIR + filename;
+        const post = fs.readFileSync(filePath, "utf-8");
+        ret.push(new Post(post));
+    });
+    return ret;
+}
+
+const posts = loadPosts();
+
+export default {
     plugins: [
         createEjsPlugin({
             author: "ForYes",
             bio: "Make it Awesome",
             sitetitle: "ForYes",
             subtitle: "Years rolling by just like a dream",
-            posts: [
-                { title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you" },
-                { title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you" },
-                { title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you" },
-                { title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you" },
-                { title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you" },
-                { title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you" },
-                { title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you" },
-                { title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you" },
-                { title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you" },
-                { title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you" },
-                { title: "fuck", tags: ["you"], date: "2020", preview: "i want to fuck you" },
-            ],
-            postNumber: 0,
-            tagNumber: 0,
-            dayNumber: 0,
+            posts: posts,
+            postNumber: posts.length,
+            tagNumber: 2,
+            dayNumber: 2,
             lastUpdateDate: "2026/1/1",
         }),
-        viteSingleFile()
+        createSinglefilePlugin(),
     ],
     build: {
         outDir: "./docs",
     },
-});
+};
